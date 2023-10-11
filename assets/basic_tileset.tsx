bd77d42981920d14dc7140782574c1431fb662f2
Buguidean <buguidean@gmail.com>
2023-10-11 21:08:16 +0200

    Library actualization

--- assets/basic_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="basic_tileset" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="basic_tileset.png" width="64" height="64"/>
-</tileset>
